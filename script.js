1a75b20563a7360fb7f29339fe7f64eca9e21080
Sina Arzany <sina@arzany.net>
2018-11-23 22:49:58 +0100

    add custom css for input time and hide timer onload

--- script.js
+++ script.js
@@ -1,41 +1,35 @@
 var secondsRemaining;
 var intervalHandle;
 
-function reserPage() {
+// to reset page after countdown
+function resetPage() {
     document.getElementById("inputArea").style.display = "block";
 }
 
 function tick() {
-    // grab the h1
     var timeDisplay = document.getElementById("time");
-    
     // turn seconds into mm:ss
     var min = Math.floor(secondsRemaining / 60);
     var sec = secondsRemaining - (min * 60);
-    
     // add a leading zero (as a string value) if seconds less than 10
     if (sec < 10) {
         sec = "0" + sec;
     }
-    // concatenate with colon
     var message = min + ":" + sec;
-    // now change the display
     timeDisplay.innerHTML = message;
     
-    // stop if down to zero
     if (secondsRemaining === 0) {
         alert("Done!");
         clearInterval(intervalHandle);
         resetPage();
     }
-    // subtract from seconds remaining
+
     secondsRemaining--;
 }
 
 function startCountdown() {
-    // get contents of the "minutes" text box
+    document.getElementById("time").style.display = 'block';
     var minutes = document.getElementById("minutes").value;
-    // check if not a number
     if (isNaN(minutes)) {
         alert("Please enter a number!");
         return;
@@ -48,12 +42,16 @@ function startCountdown() {
     document.getElementById("inputArea").style.display = "none";
 }
 
-// as soon as the page is loaded...
+// typical window.onload , as soon as the page is loaded...
 window.onload =  function () {
     // create input text box and give it an id of "minutes"
     var inputMinutes = document.createElement("input");
-    inputMinutes.setAttribute("id", "minutes");
     inputMinutes.setAttribute("type", "text");
+    inputMinutes.setAttribute("id", "minutes");
+    inputMinutes.setAttribute("required", "");
+    inputMinutes.setAttribute("class", "question");
+    inputMinutes.setAttribute("autocomplete", "off");
+
     // create a button
     var startButton = document.createElement("input");
     startButton.setAttribute("type", "button");
@@ -62,6 +60,7 @@ window.onload =  function () {
         startCountdown();
     };
     // add to the DOM, to the div called "inputArea"
-    document.getElementById("inputArea").appendChild(inputMinutes);
+    document.getElementById("inputArea").prepend(inputMinutes);
     document.getElementById("inputArea").appendChild(startButton);
+    document.getElementById("time").style.display = 'none';
 };
